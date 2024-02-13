af843134d68a1a1bba0ef49911a25baa33e401e7
Betsy <132311670+betsymao@users.noreply.github.com>
2024-02-13 23:52:06 +1100

    updated page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 // components
 import About from '../components/sections/About';
 import Projects from '../components/sections/Projects';
+import Contact from '../components/sections/Contact';
 
 function Home() {
   return (
     <>
       <About />
       <Projects />
+      <Contact />
     </>
   );
 }
