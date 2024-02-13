966a0a11c99a64ca9a02dd99cd761f07098aeded
Betsy <132311670+betsymao@users.noreply.github.com>
2024-02-14 10:23:01 +1100

    added react-type-animation

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,13 +1,26 @@
+import { TypeAnimation } from 'react-type-animation';
+
 function About() {
   return (
     <>
       <section id="about" className="child">
         
         <h1><span className="declaration">const</span> <span className="variable">developer</span> = <span className="value">"betsy mao"</span>;</h1>
-        
+
         <div className="comment">
-          <p>// think of something to do with the Date.now() method</p>
-          <p>// {Date.now()}</p>
+
+          <p>// to do</p>
+
+          <TypeAnimation
+            sequence={[
+              '// work on',
+              1000,
+              '// work on fixing bugs',
+              1000,
+            ]}
+            speed={40}
+            repeat={0}
+          />
         </div>
         
       </section>
