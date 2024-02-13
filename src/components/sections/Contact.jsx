9d808ecd0d078704e77ee638d0d9287f391b3456
Betsy <132311670+betsymao@users.noreply.github.com>
2024-02-14 10:52:21 +1100

    updated contact component

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
         <h2><span className="declaration">const</span> <span className="variable">contact</span>;</h2>
 
         <div className="comment">
-          <p>// i can be contacted via <a href="https://www.linkedin.com/in/maobetsy" className="comment__link">linkedin</a></p>
+          <p>// send me a message via <a href="https://www.linkedin.com/in/maobetsy" className="comment__link">linkedin</a></p>
         </div>
       
       </section>
