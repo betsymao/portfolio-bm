b52743686b066ca3bc83391f6573c1a0f048b400
Betsy <132311670+betsymao@users.noreply.github.com>
2024-02-13 23:52:43 +1100

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
+// libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+// application
 import App from './App.jsx';
 
-import { BrowserRouter } from 'react-router-dom';
+// stylesheet
+import './css/main.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
